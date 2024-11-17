79d8e51dfbb2000799b6751e8062a0624562974a
takonef <takonef@gmail.com>
2024-11-17 10:37:26 +0300

    Update main.py

--- radio2stepindividual/main.py
+++ radio2stepindividual/main.py
@@ -1,4 +1,13 @@
-def proceed(data: str) -> bytes:
-  a=[]
-  return a
+import numpy as np
+
+string = 'babah'
+def proceed(stri):
+    arr = "".join(f"{ord(i):08b}" for i in stri)
+    arr = np.array(list(arr))
+    print(arr)
+    return np.array(arr, dtype = 'bool')
+
+print(type(proceed(string)))
+print(np.array(proceed(string), dtype=int))
+
 # babah
