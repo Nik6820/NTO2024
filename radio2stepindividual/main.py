bc6919af25ba64bc2fe27f5578d7ad479c641272
takonef <takonef@gmail.com>
2024-11-25 19:49:02 +0300

    Update main.py

--- radio2stepindividual/main.py
+++ radio2stepindividual/main.py
@@ -1,14 +1,59 @@
 import numpy as np
 
-string = 'babah'
-def proceed(stri):
-    arr = "".join(f"{ord(i):08b}" for i in stri)
-    # че-то я вообще не поняла, во что оно в принципе закодировалось...
-    arr = np.array(list(arr))
-    print(arr)
-    return np.array(arr, dtype = 'bool')
+def proceed(data: str) -> bytes:
+    out=[]
+    data_num = [ord(c) for c in data]
+    csum = []
+    out= [0] + [255] + [255] + out + data_num + csum + [0] + [255] + [255] + [255]
+    return bytes(out)
 
-print(type(proceed(string)))
-print(np.array(proceed(string), dtype=int))
+import numpy as np
+def decod(inp, buf, end):
+    buf = np.append(buf, inp)
+    out = ''
 
-# babah
+    summ = 0
+    st_ind = 0
+    for i in range(len(buf)):
+        if buf[i]:
+            summ += 1
+        else:
+            summ = 0
+        if summ == 16:
+            st_ind = i + 1 - summ
+            break
+    buf = buf[st_ind:]
+    summ = 0
+
+    fin_ind = 0
+    for i in range(24, len(buf)):
+        if buf[i]:
+            summ += 1
+        else:
+            summ = 0
+        if summ == 24:
+            fin_ind = i + 1 - summ - 8
+            break    
+
+    if fin_ind != 0:
+        out = ''.join(buf[16:fin_ind].astype(int).astype(str).tolist())
+
+        binary_int = int(out, 2)
+        byte_number = binary_int.bit_length() + 7 // 8
+        binary_array = binary_int.to_bytes(byte_number, "big")
+        ascii_text = binary_array.decode('ascii')
+
+        buf = buf[(fin_ind + 24):]
+        return ascii_text
+    else:
+        return ''
+
+# Теоретически что с данным делает орбита
+buf = np.array([], bool)
+binary_strings = ['{:08b}'.format(byte) for byte in proceed(input())]
+arr = []
+for i in ''.join(binary_strings):
+    arr += [int(i)]
+pl = np.array(([1, 0, 1, 1] + arr + [1, 0, 1]), bool) 
+print(decod(pl, buf, 1))
+
