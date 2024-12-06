53a23f3cf592187d17bde7459491310b5ff90ef3
Nik6820 <152095020+Nik6820@users.noreply.github.com>
2024-12-06 17:22:05 +0300

    Update firstteamstation_simple.js

--- firstteamstation_simple.js
+++ firstteamstation_simple.js
@@ -1,1 +1,15 @@
+let transmitter;
+let receiver;
 
+function setup() {
+  transmitter = spacecraft.devices[0].functions[0];
+  receiver = spacecraft.devices[1].functions[0];
+  let count=0;
+}
+function loop() {
+    let packet = receiver.receive(20);
+        // Место для обработки сообщения
+  if (count%3 === 0){
+    transmitter.transmit(packet);
+  }
+}
