7a08a2d04466a762fc25967aaa2992ddda2d6f5d
Nik6820 <152095020+Nik6820@users.noreply.github.com>
2024-12-07 20:36:57 +0300

    Update firstteam.js

--- firstteam.js
+++ firstteam.js
@@ -6,11 +6,12 @@ var gyros;
 var nav;
 var transmitter;
 var receiver;
-let kp=0;
-let ki=0;
-let ts=0;
-
-let umax=0.0001; //беда какая то, какой максимум?
+let kp=-1;
+let ki=1;
+let ts=0.01;
+let LaKyaka=[-22.1023, 294.40701-180];
+let Honkong=[22.28552, 114.15769-180];
+let umax=0.0001;
 let I=0;
 
 function anglength (phix, phiin, lambdax, lambdain)
@@ -61,8 +62,24 @@ function loop()
   u = minmax(u, -umax, umax)
   wheels.functions[0].motor_torque = u;
   //end stab
-  let received_packet = receiver.receive(10);
-
-  transmitter.transmit(received_packet);
+  let coords=nav.location(3);
+  let phi=coords[0];
+  let lambda=coords[1];
+  if (Math.abs(phi-LaKyaka[0])<20 && Math.abs(lambda-LaKyaka[1])<20)
+  {
+     if (anglength(phi, LaKyaka[0], lambda, LaKyaka[1])<15.5)
+     {
+        let received_packet = receiver.receive(26);//тут прием и проверка пакетов
+        
+     }
+  }
+  if (Math.abs(phi-Honkong[0])<20 && Math.abs(lambda-Honkong[1])<20)
+  {
+     if (anglength(phi, Honkong[0], lambda, Honkong[1])<15.5)
+     {
+        transmitter.transmit(received_packet);//тут отправка пакетов
+        
+     }
+  }
 }
 
