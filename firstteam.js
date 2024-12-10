d0b5d6f6e2554cabfd02d4f9d398b38f8e11d678
Nik6820 <152095020+Nik6820@users.noreply.github.com>
2024-12-10 16:40:18 +0300

    Update firstteam.js

--- firstteam.js
+++ firstteam.js
@@ -10,8 +10,11 @@ var receiver;
 let kp=-60;
 let ki=3;
 let ts=0.01;
-//let LaKyaka=[-22.1023, 294.40701-360];
-//let Honkong=[22.28552, 114.15769];
+let LaKyaka=[-22.1023, 294.40701-180];
+let Honkong=[22.28552, 114.15769-180];
+let coords;
+let lambda;
+let phi;
 let umax=0.0001;
 let I=0;
 let rot_speed_x;
@@ -24,7 +27,8 @@ var buf = new Array();
 var trans = new Uint8Array(20);
 // transmit
 var sent = 0;
-var mail=[];
+let time;
+
 
 function minmax (a,min,max)
 {
@@ -60,45 +64,44 @@ function loop()
   err=0-ang_speed_x;
   P=err*kp;
   u=P+I;
-  if (u === minmax(u, -umax, umax)){
+  if (u == minmax(u, -umax, umax)){
     I = I + err*ki*ts;
     I=I*kp;
   }
   u = minmax(u, -umax, umax);
   wheels.functions[0].motor_torque = u;
   //end stab
+   
   ////////// RECIEVER ///////////
-     if (buf.length < 51000)
+  if (buf.length < 51000)
   { 
-        let packet = receiver.receive(23);
-        if (packet.size === 23)
+     let packet = Array.from(receiver.receive(23));
+     if (packet.size === 23)
+     {
+        let sym = 0;
+      
+        // проверка помех в данных о пакете
+        if (packet[20] !== packet[21] && packet[20] !== packet[22]) 
         {
-           let sym = 0;
-         
-           // проверка помех в данных о пакете
-           if (packet[20] !== packet[21] && packet[20] !== packet[22]) 
-           {
-             packet[20] = packet[21];
-           } 
-           // конец проверки
-           
-           for (let i = 0; i < 20; i++) // сохраняю данные без повторов
-           {
-              trans[i] = packet[i];
-              sym = sym + packet[i];
-           }
-           if (sym%256 === packet[20]) 
-           {
-            buf.push(trans);      
-           }
-         }
+          packet[20] = packet[21];
+        } 
+        // конец проверки
+        
+        for (let i = 0; i < 20; i++) // сохраняю данные без повторов
+        {
+           trans[i] = packet[i];
+           sym = sym + packet[i];
+        }
+        if (sym%256 === packet[20]) 
+        {
+         buf.push(trans);      
+        }
+     }
   }
   ///////// TRANSMITTER //////////
-  nav = spacecraft.devices[3].functions[0];
-  let coords=nav.location;
-  if (Math.abs(coords[0] - 22.28552)  < 5 && Math.abs(coords[1] - 114.15769) < 5 && sent < buf.length && Math.abs(ang_speed_x) < 0.0003)
-  {
-      mail=buf.pop();
-      transmitter.transmit(new Uint8Array(mail);
-   }
+     if (Math.abs(ang_speed_x) < 0.0003 && (9600<=spacecraft.flight_time<=10800 || 26520<=spacecraft.flight_time<=27720))//данные по выводам из орбиты, если по gmat, то сдвигать на минут 9 назад
+     {
+        let tr=sent.pop();
+        transmitter.transmit(new Uint8Array(tr));
+     }
 }
