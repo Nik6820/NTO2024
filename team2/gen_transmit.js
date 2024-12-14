1829f7c7ff0db6e610c9b4f4a92abcc4d1566099
takonef <takonef@gmail.com>
2024-12-14 19:59:57 +0300

    Update gen_transmit.js

--- team2/gen_transmit.js
+++ team2/gen_transmit.js
@@ -1,6 +1,73 @@
 // ретранслятор
 'use strict';
 
+function encode(bytes) {
+    // encode
+}
+
+function bitsToBytes(bits) {
+    let byteArray = new Array(Math.ceil(bits.length / 8));
+    for (let i = 0; i < bits.length; i++) {
+        byteArray[Math.floor(i / 8)] |= bits[i] << (7 - (i % 8));
+    }
+    return byteArray;
+}
+
+var transmitter;
+var receiver;
+var buffer = new Array()
+
+function setup() 
+{
+  transmitter = spacecraft.devices[0].functions[0];
+  receiver = spacecraft.devices[1].functions[0];
+}
+
+function loop() 
+{
+    let received = new Uint8Array(receiver.receive(80));
+    buffer = buffer.concat(bitsToBytes(received));
+    let packet = new Uint8Array(buffer.splice(0,10))
+    transmitter.transmit(encode(packet));
+}
+
+
+/////////////////// станция /////////////////////
+'use strict';
+
+function bitsToBytes(bits) {
+    let byteArray = new Array(Math.ceil(bits.length / 8));
+    for (let i = 0; i < bits.length; i++) {
+        byteArray[Math.floor(i / 8)] |= bits[i] << (7 - (i % 8));
+    }
+    return byteArray;
+}
+
+function decode(bits) {
+    // decode
+}
+
+var transmitter;
+var receiver;
+var buffer = new Array()
+
+function setup() 
+{
+  transmitter = spacecraft.devices[0].functions[0];
+  receiver = spacecraft.devices[1].functions[0];
+}
+function loop() 
+{
+  let received = new Uint8Array(receiver.receive(80));
+  let decoded = decode(received);
+  buffer = buffer.concat(bitsToBytes(decoded));
+  transmitter.transmit(new Uint8Array(buffer.splice(0,10)));
+}
+
+
+// ретранслятор
+'use strict';
+
 function bitsToBytes(bits) {
     let byteArray = new Array(Math.ceil(bits.length / 8));
     for (let i = 0; i < bits.length; i++) {
@@ -24,45 +91,6 @@ function setup()
 function loop() 
 {
   let received = new Uint8Array(receiver.receive(80)); // в научном тут штука с камеры))))
-  // let decoded = decode(received);
-  // let packet = bitsToBytes(decoded);
   buffer = buffer.concat(bitsToBytes(received));
-  transmitter.transmit(new Uint8Array(buffer.splice(0,10)));
-}
-
-
-// станция
-'use strict';
-
-function bitsToBytes(bits) {
-    let byteArray = new Array(Math.ceil(bits.length / 8));
-    for (let i = 0; i < bits.length; i++) {
-        byteArray[Math.floor(i / 8)] |= bits[i] << (7 - (i % 8));
-    }
-    return byteArray;
-}
-
-function encode(bytes) {
-    // encode
-}
-
-function decode(bits) {
-    // decode
-}
-
-var transmitter;
-var receiver;
-var buffer = new Array()
-
-function setup() 
-{
-  transmitter = spacecraft.devices[0].functions[0];
-  receiver = spacecraft.devices[1].functions[0];
-}
-function loop() 
-{
-  let received = new Uint8Array(receiver.receive(80)); // в научном тут штука с камеры))))
-  let decoded = decode(received);
-  buffer = buffer.concat(bitsToBytes(decoded));
   transmitter.transmit(new Uint8Array(buffer.splice(0,10)));
 }
