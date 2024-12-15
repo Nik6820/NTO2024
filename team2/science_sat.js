f8132fca92bbdb2e80996db4d5f29833ebba93de
takonef <takonef@gmail.com>
2024-12-15 11:03:53 +0300

    Update science_sat.js

--- team2/science_sat.js
+++ team2/science_sat.js
@@ -5,20 +5,18 @@ var storage;
 var sun_sensor;
 var camera;
 var transmitter;
-var receiver;
 
 function zip(picture) {
     let buffer = new ArrayBuffer();
     for (var i = 0; i < 20; i++) {
         for (var j = 0; j < 20; j++) {
-            let a1=picture[j*2+i*80];
-            let a2=picture[j*2+i*80+1];
-            let a3=picture[j*2+i*80+40];
-            let a4=picture[j*2+i*80+41];
+            let a1 = picture[j*2+i*80];
+            let a2 = picture[j*2+i*80+1];
+            let a3 = picture[j*2+i*80+40];
+            let a4 = picture[j*2+i*80+41];
             buffer.push(Math.round((a1+a2+a3+a4)/4));
         }
     }
-
     return new Uint8Array(buffer);
 }
 
@@ -32,6 +30,7 @@ function tofloat32(floatValue) {
     // Возвращаем массив из 4 байт
     return new Uint8Array(view.buffer);
 }
+
 function toint32(sensorData) {
     // Проверяем, что входной параметр n соответствует ожидаемому размеру
     // Создаем новый DataView из Uint8Array
@@ -46,6 +45,7 @@ function toint32(sensorData) {
     // Возвращаем результат как массив из двух чисел
     return [x, y];
 }
+
 function calculateSunDirection(xPixel, yPixel) {
     const WidthMM = 22.3;
     const HeightMM = 14.9;
@@ -62,36 +62,34 @@ function calculateSunDirection(xPixel, yPixel) {
     const z = -Z / distance;
   
     //return [x.toFixed(3) * 1, y.toFixed(3) * 1, z.toFixed(3) * 1];
-  return Math.acos(z)*180/Math.PI;
+    return Math.acos(z)*180/Math.PI;
   }
-
 
 
 function setup() {
     transmitter = spacecraft.devices[0].functions[0];
-    receiver = spacecraft.devices[1].functions[0];
     camera = spacecraft.devices[2].functions[0];
     sun_sensor = spacecraft.devices[3].functions[0];
     storage = spacecraft.devices[4].functions[0];    
 }
-let packets=[];
+
+let photos = new Array();
 function loop() {    
-  let data_sensor=new Uint8Array(sun_sensor.read(16));
-  let vec_sens=toint32(data_sensor);
-  if (vec_sens[0]!==-1 && vec_sens[1]!==-1){
+    let data_sensor = new Uint8Array(sun_sensor.read(16));
+    let vec_sens = toint32(data_sensor);
+    if (vec_sens[0] !== -1 && vec_sens[1] !== -1){
     let angle = calculateSunDirection(vec_sens[0],vec_sens[1]);
-    if (180-angle<=15){
-      pic=camera.read(1600);
-      zippic = zip(pic);
-      angle32=tofloat32(angle);
-      let data = new Uint8Array([...zippic, ...angle32]);
-      storage.write(data);
-      packets.push(zippic); // "че по формату", - Татьяна -- по формату Uint8Array
+    if (180-angle <= 15){
+        pic = camera.read(1600);
+        zippic = zip(pic); // сжатая картинка камеры
+        angle32 = tofloat32(angle);
+        let data = new Uint8Array([...zippic, ...angle32]);
+        storage.write(data);
+        photos.push(zippic); // "че по формату", - Татьяна -- по формату Uint8Array
     }
-  }
-  if (){//на сближение
-    pack=data.pop();
-    transmitter.transmit(pack);
+    }
+    if (){//на сближение
+        transmitter.transmit(photos.pop()); // !!ЕСЛИ МОЖНО ЗА РАЗ 400 БАЙТ
   
   }
 }
