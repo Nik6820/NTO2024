74cb3099b160312d2bccb151009c93b6bb9e9a21
Nik6820 <152095020+Nik6820@users.noreply.github.com>
2024-12-15 07:41:03 +0300

    Update reedsolomon.js

--- team2/reedsolomon.js
+++ team2/reedsolomon.js
@@ -37,3 +37,64 @@ function init_tables(prim = 0x11d) {
 
     return [gf_log, gf_exp];
 }
+
+function gf_mul(x,y){
+  if (x===0 || y===0){
+    return 0;
+  }
+  return gf_exp[(gf_log[x]+gf_log[y])%255];
+}
+
+function gf_div(x,y){
+  if (x===0){
+    return 0;
+  }
+  return gf_exp[(gf_log[x] + 255 - gf_log[y]) % 255];
+}
+
+function gf_pow(x, power){
+  return gf_exp[(gf_log[x] * power) % 255];
+}
+
+function gf_inverse(x){
+    return gf_exp[255 - gf_log[x]];
+}
+
+//now polynomials...
+
+function gf_poly_scale(p,x){
+  let r = new Array(p.length).fill(0);
+  for (let i=0;i<p.length;i++){
+    r[i] = gf_mul(p[i], x)
+  }
+  return r;
+}
+
+function gf_poly_add(p,q){
+  let r = new Array(Math.max(p.length,q.length)).fill(0);
+  for (let i=0; i<p.length;i++){
+        r[i+len(r)-len(p)] = p[i];
+  }
+  for (let i=0; i<q.length;i++){
+        r[i+len(r)-len(q)] = r[i+len(r)-len(q)]^q[i];
+  }
+  return r
+}
+
+function gf_poly_mul(p,q){
+  let r = new Array(p.length+q.length-1).fill(0);
+  for (let j=0; j<q.length;j++){
+    for (let i=0; i<p.length;i++){
+      r[i+j] = r[i+j]^gf_mul(p[i], q[j]);
+    }
+  }
+  return r;
+}
+
+function gf_poly_eval(poly, x){
+  let y = poly[0];
+  for (let i=0; i<poly.length;i++){
+      y = gf_mul(y, x) ^ poly[i];
+  }
+  return y
+}
