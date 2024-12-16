3e8af0a51e8c2e5cb18612fc6ed5b7f648ad0cdb
takonef <takonef@gmail.com>
2024-12-16 18:43:03 +0300

    Create station.js

---
+++ team2/station.js
@@ -0,0 +1,33 @@
+/////////////////// станция /////////////////////
+'use strict';
+
+function bitsToBytes(bits) {
+    let byteArray = new Array(Math.ceil(bits.length / 8));
+    for (let i = 0; i < bits.length; i++) {
+        byteArray[Math.floor(i / 8)] |= bits[i] << (7 - (i % 8));
+    }
+    return byteArray;
+}
+
+function decode(bits) {
+    // decode - если РС, аргумент поменять на байты!
+}
+var lencod = 0 // количество символов на кодировку
+
+var transmitter;
+var receiver;
+var buf = new Array()
+
+function setup() 
+{
+    transmitter = spacecraft.devices[0].functions[0];
+    receiver = spacecraft.devices[1].functions[0];
+}
+
+function loop() 
+{
+    let received = new Uint8Array(receiver.receive(80)); 
+    let decoded = decode(received);
+    buf = buf.concat(bitsToBytes(decoded)); //конкатенация массивов в фотку, если там РС - убрать битс ту байтс
+    transmitter.transmit(new Uint8Array(buf.splice(0,10))); // мы в очке?? В зависимости от ответа орбиты выводим разное. Мб 400 байт за раз.
+}
